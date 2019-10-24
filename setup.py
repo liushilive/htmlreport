c14de9f4ee72038e79a986a933952d0d9b96242d
刘士 <liushilive@outlook.com>
2019-10-24 16:58:23 +0800

    升级版本号

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Natural Language :: Chinese (Simplified)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ]
